b2ba636cd6cb789019895d7a4d49a470debff1ce
Pablo Castellazzi <pablo.castellazzi@icloud.com>
2019-09-30 16:41:35 -0300

    remove AWS profile handling from the prompt

--- config/fish/functions/fish_prompt.fish
+++ config/fish/functions/fish_prompt.fish
@@ -1,20 +1,8 @@
 function fish_prompt
   echo -s (set_color green) (prompt_pwd)
 
-  if set -q AWS_PROFILE
-    set my_aws_profile $AWS_PROFILE
-  end
-
-  if set -q AWS_VAULT
-    set my_aws_profile $AWS_VAULT
-  end
-
-  if set -q AUTODESK_ENVIRONMENT
-    set my_aws_profile $AUTODESK_ENVIRONMENT
-  end
-
-  if set -q my_aws_profile
-    echo -n -s (set_color blue) "{$my_aws_profile} "
+  if set -q AWS_ENVIRONMENT
+    echo -n -s (set_color blue) "{AWS_ENVIRONMENT} "
   end
 
   echo -n -s (set_color yellow) (__fish_git_prompt "[%s] ")
