42adc9d820cb25222b24314a5edd523b6a686848
Pablo Castellazzi <pablo.castellazzi@icloud.com>
2026-07-19 07:41:38 -0300

    v20260719
    * fish: cache configuration snippets

--- src/config/fish/config.fish
+++ src/config/fish/config.fish
@@ -2,42 +2,40 @@ status is-interactive; or return
 
 # sudo launchctl config user umask 077
 # sudo chfn -o other='umask=077'
-if test (umask) != "0077"
-    echo >&2 "[WRN] default umask is $(umask). Forcing 0077."
+if test $umask != '0077'
+    echo >&2 "[WRN] default umask is $umask. Forcing 0077."
     umask 0077
 end
 
-set -gx LANG "en_US.UTF-8"
-set -gx LC_ALL "en_US.UTF-8"
+set -gx COPIER_SETTINGS_PATH '~/.config/copier/settings.yml'
+set -gx EDITOR 'vim'
+set -gx FZF_DEFAULT_COMMAND 'fd --type=file --hidden --follow'
+set -gx LANG 'en_US.UTF-8'
+set -gx LC_ALL 'en_US.UTF-8'
+set -gx PAGER 'bat'
 
 set -e -Ugl PATH
-fish_add_path --path --append (
-    path resolve /snap/bin /usr/local/bin /usr/bin /bin /usr/local/sbin /usr/sbin /sbin
-)
+fish_add_path --path --append /usr/local/bin /usr/bin /bin /usr/local/sbin /usr/sbin /sbin
 
 set -e -Ugl fish_user_paths
-fish_add_path ~/.local/bin
-fish_add_path ~/bin
+fish_add_path (path resolve ~/.local/bin ~/bin)
 
+set -l brew_bin brew
 switch (uname)
 case Darwin
-    /usr/local/bin/brew shellenv | source
+    set brew_bin /usr/local/bin/brew
 case Linux
-    /home/linuxbrew/.linuxbrew/bin/brew shellenv | source
+    set brew_bin /home/linuxbrew/.linuxbrew/bin/brew
 end
 
-set -gx EDITOR vim
-set -gx PAGER bat
-set -gx COPIER_SETTINGS_PATH ~/.config/copier/settings.yml
-set -gx FZF_DEFAULT_COMMAND 'fd --type=file --hidden --follow'
-
-batman --export-env | source
-fzf --fish | source
-zoxide init fish | source
+cache_config $brew_bin shellenv
+cache_config batman --export-env
+cache_config fzf --fish
+cache_config zoxide init fish
 
 alias cat=bat
 alias ls="eza --classify=auto --color=auto --icons=auto"
 
 if test -f ~/.config/fish/config.local.fish
-    . ~/.config/fish/config.local.fish
+    source ~/.config/fish/config.local.fish
 end
---
+++ src/config/fish/functions/cache_config.fish
@@ -0,0 +1,25 @@
+function cache_config
+    if test (count $argv) -lt 1
+        echo "Error: cache_shellenv requires at least a command name." >&2
+        return 1
+    end
+
+    set -l cmd $argv[1]
+    set -l bin_path (type -p $cmd)
+    set -l bin_name (basename $bin_path)
+
+    if test -z $bin_path
+        echo "Error: Command '$cmd' not found in PATH" >&2
+        return 1
+    end
+
+    set -l cache_dir $__fish_config_dir/cache.d
+    set -l cache_file $cache_dir/$bin_name.fish
+
+    mkdir -p $cache_dir
+    if test $bin_path -nt $cache_file
+        $bin_path $argv[2..] > $cache_file
+    end
+
+    source $cache_file
+end
